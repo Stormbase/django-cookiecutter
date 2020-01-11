60f03f52c91aef047b05d523912be1d8fa2f63be
Storm Heg <storm@stormbase.co>
2020-01-12 00:01:27 +0100

    Add user admin fieldsets

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/user/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/user/admin.py
@@ -3,4 +3,32 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(AbstractUserAdmin):
+    list_display = ["__str__", "is_active", "is_staff", "is_superuser"]
+    list_filter = ["is_active", "is_staff", "is_superuser", "groups"]
+    ordering = ["email"]
+    search_fields = ["email"]
+    fieldsets = (
+        (None, {"fields": ["username", "password",]}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email",)},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": [
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ]
+            },
+        ),
+        (_("Important dates"), {"fields": ["last_login", "date_joined"]}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ["wide"], "fields": ["email", "password1", "password2"]},),
+    )
