a3a35103323904f8e3c86fe72411282de42a8583
Storm Heg <storm@stormbase.co>
2020-02-15 14:48:26 +0000

    Fix test

--- {{cookiecutter.project_slug}}/tests/integration/test_urls.py
+++ {{cookiecutter.project_slug}}/tests/integration/test_urls.py
@@ -2,8 +2,5 @@ import pytest
 
 
 def test_root_available(client, settings):
-    # The Django welcome page is only shown if DEBUG = True
-    settings.DEBUG = True
-
     response = client.get("/")
     assert response.status_code == 200
