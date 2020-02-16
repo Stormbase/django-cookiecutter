cbb04a0bd6c25cfd3879a648d1f60585a4b70407
Storm Heg <storm@stormbase.co>
2020-02-16 16:07:54 +0000

    Add media and staticfiles url patterns for development

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -143,6 +143,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+# File storage for user-uploaded files
+# https://docs.djangoproject.com/en/3.0/ref/settings/#std:setting-MEDIA_URL
+# https://docs.djangoproject.com/en/3.0/ref/settings/#std:setting-MEDIA_ROOT
 MEDIA_URL = env.str("MEDIA_URL", default="/media/")
 MEDIA_ROOT = os.path.join(BASE_DIR, "public/media")
 
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +6,13 @@ urlpatterns = [
     path("", include("{{cookiecutter.project_slug}}.hello.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
